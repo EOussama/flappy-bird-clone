6a61b55506873dadcbad4988b1d5e29dd6c06425
EOussama <oussama_gtacj@live.fr>
2018-04-15 21:53:29 +0100

    Globl initialisation

--- conf.lua
+++ conf.lua
@@ -16,18 +16,23 @@
                                                                     @Created on:    4/15/2018 - 9:26PM
 ]]
 
-function love.load()
-    print("Hello world!")
-end
 
-function love.draw()
-    
-end
+function love.conf(t)
+    -- Love2D configuration
+    t.version = '11.0'
+    t.console = true
 
-function love.update(dt)
+    -- Game information
+    t.window.title = "Flappy Bird clone"
+    t.window.icon = "assets/drawables/icon.png"
+    t.window.display = 1
+    t.window.width = 400
+    t.window.height = 600
 
-end
-
-function love.quit()
-
+    -- Modules
+    t.modules.joystick = false
+    t.modules.thread = false  
+    t.modules.touch = false   
+    t.modules.video = false   
+    t.modules.data = false    
 end
